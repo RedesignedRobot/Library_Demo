544be391304bc3339b8a98dd4479822e7d08315e
Amir Ayub <dev.amirayub@gmail.com>
2016-10-24 17:34:18 +0400

    Test class updated

--- app/src/main/java/com/example/nitrov17/library_demo/Test.java
+++ app/src/main/java/com/example/nitrov17/library_demo/Test.java
@@ -1,5 +1,7 @@
 package com.example.nitrov17.library_demo;
 
+import android.widget.Toast;
+
 /**
  * Created by NitroV17 on 10/24/2016.
  */
@@ -8,6 +10,6 @@ public class Test {
     String name;
     public void fun(String[] a)
     {
-
+        //Magic
     }
 }
