b1077c12b2b04819b346afde8eeae30b36b66d8d
Amir Ayub <dev.amirayub@gmail.com>
2016-10-24 16:08:15 +0400

    Added Test class

---
+++ app/src/main/java/com/example/nitrov17/library_demo/Test.java
@@ -0,0 +1,13 @@
+package com.example.nitrov17.library_demo;
+
+/**
+ * Created by NitroV17 on 10/24/2016.
+ */
+
+public class Test {
+    String name;
+    public void fun(String[] a)
+    {
+
+    }
+}
